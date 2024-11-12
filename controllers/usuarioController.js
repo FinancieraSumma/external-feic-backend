aa63552983cf14cd8d26aae39fa3b41a4c4c8519
Lino Garcia Vallejo <lino.garciavallejo@outlook.com>
2024-11-11 22:08:02 -0600

    Add user login and verification functionality with email notifications

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,7 +1,10 @@
-const axios = require('axios');
+const axios = require("axios");
 const Usuario = require("../core/models/Usuario");
 const BitacoraVerificacion = require("../core/models/bitacoraVerificacion");
 const generateRandomHexString = require("../core/services/cryptoService");
+const sendVerificationEmail = require("../core/services/emailService");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const RECAPTCHA_SECRET_KEY =
   process.env.RECAPTCHA_SECRET_KEY ||
@@ -39,11 +42,13 @@ exports.registerUser = async (req, res) => {
     }
     console.log("reCaptcha Verification successful");
 
-    const existingUser = await Usuario.findOne({ correoElectronico });
-    if (existingUser) {
+    const correoElectronicoYaRegistrado = await Usuario.findOne({
+      correoElectronico,
+    });
+    if (correoElectronicoYaRegistrado) {
       return res.status(400).json({
-      success: false,
-      message: "El correo electrónico ya está en uso.",
+        success: false,
+        message: "El correo electrónico ya está en uso.",
       });
     }
 
@@ -52,24 +57,32 @@ exports.registerUser = async (req, res) => {
       nit,
       correoElectronico,
       password,
-      status: 0
+      status: 0,
     });
 
     //const nuevoUsuario = new Usuario(req.body.nit, req.body.correoElectronico, req.body.contraseña);
     await nuevoUsuario.save();
 
-    const hexString = generateRandomHexString();
+    const codigoVerificacion = generateRandomHexString();
+    const secret = process.env.JWT_SECRET || 'my_fallback_secret_key';  // Replace 'your_fallback_secret_key' with a secure fallback secret key
+    const combinedToken = jwt.sign({ nit, codigoVerificacion }, secret, { expiresIn: '1d' });
 
     const bitacoraVerificacion = new BitacoraVerificacion({
       nit: nuevoUsuario.nit,
       evento: 0,
-      codigoVerificacion: hexString,
-      fechaExpiracion: Date.now() + 12 * 60 * 60 * 1000 // 12 hours,
+      codigoVerificacion,
+      fechaExpiracion: Date.now() + 24 * 60 * 60 * 1000, // 12 hours,
     });
 
     await bitacoraVerificacion.save();
 
-    res.status(201).send("Usuario registrado exitosamente.");
+    // Send email with verification code
+    sendVerificationEmail(nuevoUsuario.correoElectronico, combinedToken);
+
+    res.status(201).json({
+      success: true,
+      message: "Usuario registrado exitosamente.",
+    });
   } catch (error) {
     console.error("Error al registrar usuario:", error);
     res.status(400).send(error);
@@ -77,27 +90,112 @@ exports.registerUser = async (req, res) => {
 };
 
 exports.verifyUser = async function (req, res) {
-  const { codigoVerificacion, nit } = req.body;
-  if (!codigoVerificacion) {
+  console.log("Verificando usuario...");
+  console.log(req.body);
+  const { token } = req.body;
+  if (!token) {
     return res
       .status(400)
       .json({ success: false, message: "Código de verificación faltante" });
   }
 
   try {
-    //const usuario = await Usuario.findOne({ codigoVerificacion });
-    const usuario = await Usuario.findOne({ nit, codigoVerificacion });
-    if (!usuario) {
+    const secret = process.env.JWT_SECRET || 'my_fallback_secret_key';  // Replace 'your_fallback_secret_key' with a secure fallback secret key
+    const { nit, codigoVerificacion } = jwt.verify(token, secret);
+
+    const bitacoraVerificacion = await BitacoraVerificacion.findOne({ nit, codigoVerificacion, evento: 0 }).sort({ fechaExpiracion: -1 });
+    if (!bitacoraVerificacion) {
       return res
         .status(404)
-        .json({ success: false, message: "Usuario no encontrado" });
+        .json({ success: false, message: "Bitácora de verificación no encontrada" });
     }
 
-    usuario.verificado = true;
+    const usuario = await Usuario.findOne({ nit });
+    usuario.status = 1;
     await usuario.save();
     res.json({ success: true, message: "Usuario verificado exitosamente" });
   } catch (error) {
     console.error("Error al verificar el usuario:", error);
     res.status(500).send("Error al verificar el usuario.");
+  }
+};
+
+exports.login = async function (req, res) {
+  const { nit, password } = req.body;
+  console.log(`nit: ${nit}, contraseña: ${password}`);
+
+  try {
+    Usuario.findOne({ nit, password }, async function (err, usuario) {
+      if (err) {
+        console.error("Error al iniciar sesión:", err);
+        return res.status(500).send("Error al iniciar sesión.");
+      }
+  
+      if (!usuario) {
+        return res.status(404).json({
+          success: false,
+          message: "Correo electrónico o contraseña incorrectos.",
+        });
+      }
+  
+      if (usuario.status !== 1) {
+        return res.status(403).json({
+          success: false,
+          message: "Usuario no verificado.",
+        });
+      }
+
+      const codigoVerificacion = generateRandomHexString();
+      const secret = process.env.JWT_SECRET || 'my_fallback_secret_key';  // Replace 'your_fallback_secret_key' with a secure fallback secret key
+      const combinedToken = jwt.sign({ nit, codigoVerificacion }, secret, { expiresIn: '1d' });
+  
+      const bitacoraVerificacion = new BitacoraVerificacion({
+        nit: usuario.nit,
+        evento: 1,
+        codigoVerificacion,
+        fechaExpiracion: Date.now() + 24 * 60 * 60 * 1000, // 12 hours,
+      });
+  
+      await bitacoraVerificacion.save();
+  
+      // Send email with verification code
+      sendVerificationEmail(usuario.correoElectronico, combinedToken);
+  
+      res.json({ success: true, message: "Inicio de sesión exitoso y código de verificación enviado." });    
+    });
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    res.status(400).send(error);
+  }
+}
+
+exports.verifyLogin = async function (req, res) {
+  console.log("Verificando login del usuario...");
+  console.log(req.body);
+  const { token } = req.body;
+  if (!token) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Código de verificación faltante" });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET || 'my_fallback_secret_key';  // Replace 'your_fallback_secret_key' with a secure fallback secret key
+    const { nit, codigoVerificacion } = jwt.verify(token, secret);
+
+    const bitacoraVerificacion = await BitacoraVerificacion.findOne({ nit, codigoVerificacion, evento: 1 }).sort({ fechaExpiracion: -1 });
+    if (!bitacoraVerificacion) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Bitácora de verificación no encontrada" });
+    }
+
+    const usuario = await Usuario.findOne({ nit });
+    usuario.status = 1;
+    await usuario.save();
+    res.json({ success: true, message: "Login de usuario verificado exitosamente" });
+  } catch (error) {
+    console.error("Error al verificar el login del usuario:", error);
+    res.status(500).send("Error al verificar el login del usuario.");
   }
 };
--- core/services/emailService.js
+++ core/services/emailService.js
@@ -1,26 +1,45 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: 'your-email@gmail.com',
-    pass: 'your-email-password'
-  }
+    user: "lagv.ales@gmail.com",
+    pass: "fonu vuwy brms vbtu",
+  },
 });
 
-exports.sendVerificationEmail = (email, codigoVerificacion) => {
+function sendVerificationEmail(email, codigoVerificacionCombinado) {
   const mailOptions = {
-    from: 'your-email@gmail.com',
+    from: "lagv.ales@gmail.com",
     to: email,
-    subject: 'Código de Verificación',
-    text: `Hola, gracias por registrarte. Tu código de verificación es: ${codigoVerificacion}. Visita http://localhost:4200/verify para completar tu registro.`
+    subject: "Verifica tu Cuenta - Código de Verificación",
+    text: `Hola,
+
+Gracias por registrarte en nuestra plataforma. Para completar tu registro, necesitamos que verifiques tu cuenta.
+
+A continuación, encontrarás tu código de verificación. Por favor, cópialo y pégalo en la página de verificación:
+
+Código de Verificación: ${codigoVerificacionCombinado}
+
+Para verificar tu cuenta, visita el siguiente enlace:
+http://localhost:4200/verify
+
+Este código de verificación es válido por 24 horas. Si no completas la verificación en este tiempo, deberás contactar al equipo de soporte para solventar el problema.
+
+Si no solicitaste esta cuenta, por favor ignora este mensaje.
+
+Gracias,
+El Equipo de Soporte.
+`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Error al enviar el correo electrónico:', error);
+      console.error("Error al enviar el correo electrónico:", error);
     } else {
-      console.log('Correo electrónico enviado:', info.response);
+      console.log("Correo electrónico enviado:", info.response);
     }
   });
-};
+}
+
+module.exports = sendVerificationEmail;
